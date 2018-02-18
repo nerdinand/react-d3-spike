22ca212584a024f0601987b33d6d8f720dd03c01
nerdinand <nerdinand@nerdinand.com>
2018-02-18 14:01:45 +0100

    Sum histogram demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import BarChart from './BarChart'
+import HistogramChart from './HistogramChart'
 
 class App extends Component {
    render() {
     return (
       <div>
-        <BarChart data={[243,522,477,143,244,123,73,24,35]} size={[500,240]} />
+        <HistogramChart data={[243,522,477,143,244,123,73,24,35]} size={[500,300]} />
       </div>
     )
    }
